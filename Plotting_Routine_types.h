10c966fc142f10ea1baffaa33f97d885ef669644
Dominik <dominik.englhauser@t-online.de>
2025-06-15 18:03:10 +0200

    Addition of Plotting_Routine and Testing_Routine. Empty so far

---
+++ Plotting_Routine_types.h
@@ -0,0 +1,22 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: Plotting_Routine_types.h
+//
+// MATLAB Coder version            : 25.1
+// C/C++ source code generated on  : 15-Jun-2025 17:44:26
+//
+
+#ifndef PLOTTING_ROUTINE_TYPES_H
+#define PLOTTING_ROUTINE_TYPES_H
+
+// Include Files
+#include "rtwtypes.h"
+
+#endif
+//
+// File trailer for Plotting_Routine_types.h
+//
+// [EOF]
+//
---
+++ Testing_Procedures.cpp
@@ -0,0 +1,51 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: Testing_Procedures.cpp
+//
+// MATLAB Coder version            : 25.1
+// C/C++ source code generated on  : 15-Jun-2025 17:57:53
+//
+
+// Include Files
+#include "Testing_Procedures.h"
+
+// Function Definitions
+//
+// Arguments    : void
+// Return Type  : void
+//
+void Plotting_Routine()
+{
+}
+
+//
+// Arguments    : void
+// Return Type  : void
+//
+void Testing_Procedures()
+{
+}
+
+//
+// Arguments    : void
+// Return Type  : void
+//
+void Testing_Procedures_initialize()
+{
+}
+
+//
+// Arguments    : void
+// Return Type  : void
+//
+void Testing_Procedures_terminate()
+{
+}
+
+//
+// File trailer for Testing_Procedures.cpp
+//
+// [EOF]
+//
