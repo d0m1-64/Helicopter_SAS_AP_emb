848f8610425709dad26fa817e54c04a2ae1c9ff9
Dominik <dominik.englhauser@t-online.de>
2025-06-16 12:02:18 +0200

    Visual Improvements SIM_Main.cpp

--- SIM_Main.cpp
+++ SIM_Main.cpp
@@ -16,6 +16,7 @@ using json = nlohmann::json;
  * Main Simulation Function
  */
 int main() {
+
     // 0. Read Sim_Config.json and Init h5 Dataset
     std::ifstream file("Sim_Config.json");
     json sim_config;
@@ -29,22 +30,28 @@ int main() {
     init_dataset();
     save_config();
 
+
     // 1. gRPC client
     FlightModelClient client(grpc::CreateChannel(hel_sim_address, grpc::InsecureChannelCredentials()));
+
 
     // 2. Controller instance
     Full_Controller controller;
     controller.initialize();
 
+
     // 3. Form inputs
     auto [pilot_controls, autopilot_controls] = testing_procedures();
     save_inputs(autopilot_controls, pilot_controls);
 
+
     // 4. Initial States
     // ToDo: Define initial states
 
+
     // 5. Run the Simulation Loop
     for (int step = 0; step < NofSteps; step++) {
+
 
         // 5.1. Step the controller
         // ToDo: fix references
@@ -56,25 +63,32 @@ int main() {
                         &v, &p, &phi, &r,
                         &output_delt, &output_de_g, &output_de_c, &output_de_e);
 
+
         // 5.2. Create gRPC message from controller outputs
         auto control_msg = CreateControlMessage(output_de_c, output_de_g, output_delt, output_de_e);
 
-        // 5.3. Call gRPC
+
+        // 5.3. Step the Helicopter Model
         FlightModelOutput result = client.StepSimulation(control_msg);
+
 
         // 5.4. Update local Helicopter State
         // ToDo
+
 
         // 5.5. Save Outputs
         // ToDo
         save_timestep();
 
+
         // 5.6. Log Simulation Progress
         std::cout << "Step " << step << " of " << NofSteps << " complete\n";
     }
 
+
     // 6. Terminate controller and gRPC connection
     controller.terminate();
+
 
     // 7. Plot results
     //Plotting_Routine();
